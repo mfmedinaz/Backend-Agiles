04bdb994f2c03baea4f09d82ae4e87e07d0e6a29
Mario <mf.medinaz@uniandes.edu.co>
2022-11-06 15:47:46 -0500

    pls dont explode

--- vistas/vistas.py
+++ vistas/vistas.py
@@ -3,8 +3,18 @@ from flask_jwt_extended import jwt_required, create_access_token
 from flask_restful import Resource
 from sqlalchemy.exc import IntegrityError
 
-from modelos import db, Apuesta, ApuestaSchema, Usuario, UsuarioSchema, Carrera, CarreraSchema, CompetidorSchema, \
-    Competidor, ReporteSchema
+from modelos import (
+    db,
+    Apuesta,
+    ApuestaSchema,
+    Usuario,
+    UsuarioSchema,
+    Carrera,
+    CarreraSchema,
+    CompetidorSchema,
+    Competidor,
+    ReporteSchema,
+)
 
 apuesta_schema = ApuestaSchema()
 carrera_schema = CarreraSchema()
@@ -14,13 +24,18 @@ reporte_schema = ReporteSchema()
 
 
 class VistaSignIn(Resource):
-
     def post(self):
-        nuevo_usuario = Usuario(usuario=request.json["usuario"], contrasena=request.json["contrasena"])
+        nuevo_usuario = Usuario(
+            usuario=request.json["usuario"], contrasena=request.json["contrasena"]
+        )
         db.session.add(nuevo_usuario)
         db.session.commit()
         token_de_acceso = create_access_token(identity=nuevo_usuario.id)
-        return {"mensaje": "usuario creado exitosamente", "token": token_de_acceso, "id": nuevo_usuario.id}
+        return {
+            "mensaje": "usuario creado exitosamente",
+            "token": token_de_acceso,
+            "id": nuevo_usuario.id,
+        }
 
     def put(self, id_usuario):
         usuario = Usuario.query.get_or_404(id_usuario)
@@ -32,14 +47,21 @@ class VistaSignIn(Resource):
         usuario = Usuario.query.get_or_404(id_usuario)
         db.session.delete(usuario)
         db.session.commit()
-        return '', 204
+        return "", 204
+
+    def get(self, id_usuario):
+        usuario = Usuario.query.get_or_404(id_usuario)
+        db.session.delete(usuario)
+        db.session.commit()
+        return "", 204
 
 
 class VistaLogIn(Resource):
-
     def post(self):
-        usuario = Usuario.query.filter(Usuario.usuario == request.json["usuario"],
-                                       Usuario.contrasena == request.json["contrasena"]).first()
+        usuario = Usuario.query.filter(
+            Usuario.usuario == request.json["usuario"],
+            Usuario.contrasena == request.json["contrasena"],
+        ).first()
         db.session.commit()
         if usuario is None:
             return "El usuario no existe", 404
@@ -49,16 +71,17 @@ class VistaLogIn(Resource):
 
 
 class VistaCarrerasUsuario(Resource):
-
     @jwt_required()
     def post(self, id_usuario):
         nueva_carrera = Carrera(nombre_carrera=request.json["nombre"])
         for item in request.json["competidores"]:
             cuota = round((item["probabilidad"] / (1 - item["probabilidad"])), 2)
-            competidor = Competidor(nombre_competidor=item["competidor"],
-                                    probabilidad=item["probabilidad"],
-                                    cuota=cuota,
-                                    id_carrera=nueva_carrera.id)
+            competidor = Competidor(
+                nombre_competidor=item["competidor"],
+                probabilidad=item["probabilidad"],
+                cuota=cuota,
+                id_carrera=nueva_carrera.id,
+            )
             nueva_carrera.competidores.append(competidor)
         usuario = Usuario.query.get_or_404(id_usuario)
         usuario.carreras.append(nueva_carrera)
@@ -67,7 +90,7 @@ class VistaCarrerasUsuario(Resource):
             db.session.commit()
         except IntegrityError:
             db.session.rollback()
-            return 'El usuario ya tiene un carrera con dicho nombre', 409
+            return "El usuario ya tiene un carrera con dicho nombre", 409
 
         return carrera_schema.dump(nueva_carrera)
 
@@ -78,7 +101,6 @@ class VistaCarrerasUsuario(Resource):
 
 
 class VistaCarrera(Resource):
-
     @jwt_required()
     def get(self, id_carrera):
         return carrera_schema.dump(Carrera.query.get_or_404(id_carrera))
@@ -92,10 +114,12 @@ class VistaCarrera(Resource):
         for item in request.json["competidores"]:
             probabilidad = float(item["probabilidad"])
             cuota = round((probabilidad / (1 - probabilidad)), 2)
-            competidor = Competidor(nombre_competidor=item["competidor"],
-                                    probabilidad=probabilidad,
-                                    cuota=cuota,
-                                    id_carrera=carrera.id)
+            competidor = Competidor(
+                nombre_competidor=item["competidor"],
+                probabilidad=probabilidad,
+                cuota=cuota,
+                id_carrera=carrera.id,
+            )
             carrera.competidores.append(competidor)
 
         db.session.commit()
@@ -106,16 +130,18 @@ class VistaCarrera(Resource):
         carrera = Carrera.query.get_or_404(id_carrera)
         db.session.delete(carrera)
         db.session.commit()
-        return '', 204
+        return "", 204
 
 
 class VistaApuestas(Resource):
-
     @jwt_required()
     def post(self):
-        nueva_apuesta = Apuesta(valor_apostado=request.json["valor_apostado"],
-                                nombre_apostador=request.json["nombre_apostador"],
-                                id_competidor=request.json["id_competidor"], id_carrera=request.json["id_carrera"])
+        nueva_apuesta = Apuesta(
+            valor_apostado=request.json["valor_apostado"],
+            nombre_apostador=request.json["nombre_apostador"],
+            id_competidor=request.json["id_competidor"],
+            id_carrera=request.json["id_carrera"],
+        )
         db.session.add(nueva_apuesta)
         db.session.commit()
         return apuesta_schema.dump(nueva_apuesta)
@@ -126,7 +152,6 @@ class VistaApuestas(Resource):
 
 
 class VistaApuesta(Resource):
-
     @jwt_required()
     def get(self, id_apuesta):
         return apuesta_schema.dump(Apuesta.query.get_or_404(id_apuesta))
@@ -134,8 +159,12 @@ class VistaApuesta(Resource):
     @jwt_required()
     def put(self, id_apuesta):
         apuesta = Apuesta.query.get_or_404(id_apuesta)
-        apuesta.valor_apostado = request.json.get("valor_apostado", apuesta.valor_apostado)
-        apuesta.nombre_apostador = request.json.get("nombre_apostador", apuesta.nombre_apostador)
+        apuesta.valor_apostado = request.json.get(
+            "valor_apostado", apuesta.valor_apostado
+        )
+        apuesta.nombre_apostador = request.json.get(
+            "nombre_apostador", apuesta.nombre_apostador
+        )
         apuesta.id_competidor = request.json.get("id_competidor", apuesta.id_competidor)
         apuesta.id_carrera = request.json.get("id_carrera", apuesta.id_carrera)
         db.session.commit()
@@ -146,11 +175,10 @@ class VistaApuesta(Resource):
         apuesta = Apuesta.query.get_or_404(id_apuesta)
         db.session.delete(apuesta)
         db.session.commit()
-        return '', 204
+        return "", 204
 
 
 class VistaTerminacionCarrera(Resource):
-
     def put(self, id_competidor):
         competidor = Competidor.query.get_or_404(id_competidor)
         competidor.es_ganador = True
@@ -159,7 +187,9 @@ class VistaTerminacionCarrera(Resource):
 
         for apuesta in carrera.apuestas:
             if apuesta.id_competidor == competidor.id:
-                apuesta.ganancia = apuesta.valor_apostado + (apuesta.valor_apostado/competidor.cuota)
+                apuesta.ganancia = apuesta.valor_apostado + (
+                    apuesta.valor_apostado / competidor.cuota
+                )
             else:
                 apuesta.ganancia = 0
 
@@ -168,14 +198,15 @@ class VistaTerminacionCarrera(Resource):
 
 
 class VistaReporte(Resource):
-
     @jwt_required()
     def get(self, id_carrera):
         carreraReporte = Carrera.query.get_or_404(id_carrera)
         ganancia_casa_final = 0
 
         for apuesta in carreraReporte.apuestas:
-            ganancia_casa_final = ganancia_casa_final + apuesta.valor_apostado - apuesta.ganancia
+            ganancia_casa_final = (
+                ganancia_casa_final + apuesta.valor_apostado - apuesta.ganancia
+            )
 
         reporte = dict(carrera=carreraReporte, ganancia_casa=ganancia_casa_final)
         schema = ReporteSchema()
